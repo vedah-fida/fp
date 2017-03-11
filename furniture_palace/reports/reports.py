71c1ff689a14054dca597784d41d684ca1fc614a
wolf <kevinogechi798@gmail.com>
2017-03-12 00:20:24 +0300

    reports categorized to current month and previous month

--- furniture_palace/reports/reports.py
+++ furniture_palace/reports/reports.py
@@ -1,35 +1,100 @@
+from datetime import datetime
 from order.models import Order, OrderPayment
 from accounts.models import Profile
 from catalog.models import LentTool
 
 
-def get_all_complete_orders():
-    complete_orders = Order.objects.filter(complete_status=True)
+def current_month():
+    year = datetime.now().year
+    month = datetime.now().month
+    return {'year': year, 'month': month}
+
+
+def previous_month():
+    if datetime.now().month == 1:
+        pre_month = 12
+        year = datetime.now().year - 1
+    else:
+        pre_month = datetime.now().month - 1
+        year = datetime.now().year
+    return {'year': year, 'pre_month': pre_month}
+
+
+def get_all_complete_orders_current():
+    year = current_month()['year']
+    month = current_month()['month']
+    complete_orders = Order.objects.filter(complete_status=True, order_date__month=month, order_date__year=year)
     return complete_orders
 
 
-def get_carpenter_orders(user):
-    carpenter_orders = Order.objects.filter(carpenter=user, temp_carpenter=None)
+def get_all_complete_orders_previous():
+    year = previous_month()['year']
+    month = previous_month()['pre_month']
+    complete_orders = Order.objects.filter(complete_status=True, order_date__month=month, order_date__year=year)
+    return complete_orders
+
+
+def get_carpenter_orders_current(user):
+    year = current_month()['year']
+    month = current_month()['month']
+    carpenter_orders = Order.objects.filter(carpenter=user, temp_carpenter=None, order_date__month=month,
+                                            order_date__year=year)
     return carpenter_orders
 
 
-def get_carpenter_order_totals_commission(user):
+def get_carpenter_orders_previous(user):
+    year = previous_month()['year']
+    month = previous_month()['pre_month']
+    carpenter_orders = Order.objects.filter(carpenter=user, temp_carpenter=None, order_date__month=month,
+                                            order_date__year=year)
+    return carpenter_orders
+
+
+def get_carpenter_order_totals_commission_current(user):
     profile = Profile.objects.get(carpenter=user)
     carpenter_order_totals = 0
-    orders = get_carpenter_orders(user)
+    orders = get_carpenter_orders_current(user)
     for order in orders:
         carpenter_order_totals += (order.order_price * float(profile.commission / 100))
     return carpenter_order_totals
 
 
-def get_assigned_orders(user):
-    assigned_orders = Order.objects.filter(carpenter=user).exclude(temp_carpenter=None)
+def get_carpenter_order_totals_commission_previous(user):
+    profile = Profile.objects.get(carpenter=user)
+    carpenter_order_totals = 0
+    orders = get_carpenter_orders_previous(user)
+    for order in orders:
+        carpenter_order_totals += (order.order_price * float(profile.commission / 100))
+    return carpenter_order_totals
+
+
+def get_assigned_orders_current(user):
+    year = current_month()['year']
+    month = current_month()['month']
+    assigned_orders = Order.objects.filter(carpenter=user, order_date__month=month, order_date__year=year).exclude(
+        temp_carpenter=None)
     return assigned_orders
 
 
-def get_assigned_order_totals_commission(user):
+def get_assigned_orders_previous(user):
+    year = previous_month()['year']
+    month = previous_month()['pre_month']
+    assigned_orders = Order.objects.filter(carpenter=user, order_date__month=month, order_date__year=year).exclude(
+        temp_carpenter=None)
+    return assigned_orders
+
+
+def get_assigned_order_totals_commission_current(user):
     assigned_order_total = 0
-    orders = get_assigned_orders(user)
+    orders = get_assigned_orders_current(user)
+    for order in orders:
+        assigned_order_total += (float(order.order_price) * 0.01)
+    return assigned_order_total
+
+
+def get_assigned_order_totals_commission_previous(user):
+    assigned_order_total = 0
+    orders = get_assigned_orders_previous(user)
     for order in orders:
         assigned_order_total += (float(order.order_price) * 0.01)
     return assigned_order_total
@@ -42,17 +107,35 @@ def get_salary(user):
     return salary
 
 
-def total_commission(user):
-    return float(get_assigned_order_totals_commission(user)) + float(get_carpenter_order_totals_commission(user))
+def total_commission_current(user):
+    return float(get_assigned_order_totals_commission_current(user)) + float(
+        get_carpenter_order_totals_commission_current(user))
 
 
-def carpenter_gross_salary(user):
-    return float(get_salary(user)) + total_commission(user)
+def total_commission_previous(user):
+    return float(get_assigned_order_totals_commission_previous(user)) + float(
+        get_carpenter_order_totals_commission_previous(user))
 
 
-def get_orders_totals():
+def carpenter_gross_salary_current(user):
+    return float(get_salary(user)) + total_commission_current(user)
+
+
+def carpenter_gross_salary_previous(user):
+    return float(get_salary(user)) + total_commission_previous(user)
+
+
+def get_orders_totals_current():
     order_totals = 0
-    orders = get_all_complete_orders()
+    orders = get_all_complete_orders_current()
+    for order in orders:
+        order_totals += order.order_price
+    return order_totals
+
+
+def get_orders_totals_previous():
+    order_totals = 0
+    orders = get_all_complete_orders_previous()
     for order in orders:
         order_totals += order.order_price
     return order_totals
@@ -71,13 +154,17 @@ def get_salary_totals():
     return salary_totals
 
 
-def get_carpenter_commissions_subtotals():
+def get_carpenter_commissions_subtotals_current():
     commission_percentage = 2
     supervised_order_commission_percentage = 1
+    year = current_month()['year']
+    month = current_month()['month']
 
     supervised_commission_totals, carpenter_commission_totals = 0, 0
-    carpenter_orders = Order.objects.filter(complete_status=True, temp_carpenter=None)
-    supervised_orders = Order.objects.filter(complete_status=True).exclude(temp_carpenter=None)
+    carpenter_orders = Order.objects.filter(complete_status=True, temp_carpenter=None, order_date__month=month,
+                                            order_date__year=year)
+    supervised_orders = Order.objects.filter(complete_status=True, order_date__month=month,
+                                             order_date__year=year).exclude(temp_carpenter=None)
     for carpenter_order in carpenter_orders:
         carpenter_commission_totals += (carpenter_order.order_price * (commission_percentage / 100))
 
@@ -87,48 +174,160 @@ def get_carpenter_commissions_subtotals():
     return carpenter_commission_totals + supervised_commission_totals
 
 
-def get_temp_carpenter_commissions_subtotals():
+def get_carpenter_commissions_subtotals_previous():
+    year = previous_month()['year']
+    month = previous_month()['pre_month']
+    commission_percentage = 2
+    supervised_order_commission_percentage = 1
+
+    supervised_commission_totals, carpenter_commission_totals = 0, 0
+    carpenter_orders = Order.objects.filter(complete_status=True, temp_carpenter=None, order_date__month=month,
+                                            order_date__year=year)
+    supervised_orders = Order.objects.filter(complete_status=True, order_date__month=month,
+                                             order_date__year=year).exclude(temp_carpenter=None)
+    for carpenter_order in carpenter_orders:
+        carpenter_commission_totals += (carpenter_order.order_price * (commission_percentage / 100))
+
+    for supervised_order in supervised_orders:
+        supervised_commission_totals += (supervised_order.order_price * (supervised_order_commission_percentage / 100))
+
+    return carpenter_commission_totals + supervised_commission_totals
+
+
+def get_temp_carpenter_commissions_subtotals_current():
+    year = current_month()['year']
+    month = current_month()['month']
     temp_carpenter_commission_totals = 0
     temp_carpenter_commission_percentage = 3
-    assigned_orders = Order.objects.filter(complete_status=True).exclude(temp_carpenter=None)
+
+    assigned_orders = Order.objects.filter(complete_status=True, order_date__month=month,
+                                           order_date__year=year).exclude(temp_carpenter=None)
     for order in assigned_orders:
         temp_carpenter_commission_totals += (order.order_price * (temp_carpenter_commission_percentage / 100))
     return temp_carpenter_commission_totals
 
 
-def get_commissions_grand_totals():
-    return get_carpenter_commissions_subtotals() + get_temp_carpenter_commissions_subtotals()
+def get_temp_carpenter_commissions_subtotals_previous():
+    year = previous_month()['year']
+    month = previous_month()['pre_month']
+    temp_carpenter_commission_totals = 0
+    temp_carpenter_commission_percentage = 3
+    assigned_orders = Order.objects.filter(complete_status=True, order_date__month=month,
+                                           order_date__year=year).exclude(temp_carpenter=None)
+    for order in assigned_orders:
+        temp_carpenter_commission_totals += (order.order_price * (temp_carpenter_commission_percentage / 100))
+    return temp_carpenter_commission_totals
 
 
-def get_total_tool_damage_fees():
+def get_commissions_grand_totals_current():
+    return get_carpenter_commissions_subtotals_current() + get_temp_carpenter_commissions_subtotals_current()
+
+
+def get_commissions_grand_totals_previous():
+    return get_carpenter_commissions_subtotals_previous() + get_temp_carpenter_commissions_subtotals_previous()
+
+
+def get_total_tool_damage_fees_current():
+    year = current_month()['year']
+    month = current_month()['month']
     damage_fee_totals = 0
-    lent_tools = LentTool.objects.all().filter(was_damaged=True)
+    lent_tools = LentTool.objects.all().filter(was_damaged=True, lend_date__month=month, lend_date__year=year)
     for lent_tool in lent_tools:
         damage_fee_totals += lent_tool.tool.damage_fee
     return damage_fee_totals
 
 
-def get_total_storage_fees():
+def get_total_tool_damage_fees_previous():
+    year = previous_month()['year']
+    month = previous_month()['pre_month']
+    damage_fee_totals = 0
+    lent_tools = LentTool.objects.all().filter(was_damaged=True, lend_date__month=month, lend_date__year=year)
+    for lent_tool in lent_tools:
+        damage_fee_totals += lent_tool.tool.damage_fee
+    return damage_fee_totals
+
+
+def get_total_storage_fees_current():
+    year = current_month()['year']
+    month = current_month()['month']
     storage_fee_totals = 0
-    order_payments = OrderPayment.objects.all().exclude(days_in_store=0)
+    order_payments = OrderPayment.objects.filter(order__order_date__month=month, order__order_date__year=year).exclude(
+        days_in_store=0)
     for order_payment in order_payments:
         storage_fee_totals += order_payment.storage_fee
     return storage_fee_totals
 
 
-def get_total_lending_fees():
+def get_total_storage_fees_previous():
+    year = previous_month()['year']
+    month = previous_month()['pre_month']
+    storage_fee_totals = 0
+    order_payments = OrderPayment.objects.filter(order__order_date__month=month, order__order_date__year=year).exclude(
+        days_in_store=0)
+    for order_payment in order_payments:
+        storage_fee_totals += order_payment.storage_fee
+    return storage_fee_totals
+
+
+def get_total_lending_fees_current():
+    year = current_month()['year']
+    month = current_month()['month']
     lending_total_fee = 0
-    lent_tools = LentTool.objects.all().filter(returned=True)
+    lent_tools = LentTool.objects.filter(returned=True, lend_date__month=month, lend_date__year=year)
     for lent_tool in lent_tools:
         lending_total_fee += lent_tool.lending_fee
     return lending_total_fee
 
 
-def get_damaged_tools():
-    damaged_tools = LentTool.objects.all().filter(was_damaged=True)
+def get_total_lending_fees_previous():
+    year = previous_month()['year']
+    month = previous_month()['pre_month']
+    lending_total_fee = 0
+    lent_tools = LentTool.objects.all().filter(returned=True, lend_date__month=month, lend_date__year=year)
+    for lent_tool in lent_tools:
+        lending_total_fee += lent_tool.lending_fee
+    return lending_total_fee
+
+
+def get_damaged_tools_current():
+    year = current_month()['year']
+    month = current_month()['month']
+    damaged_tools = LentTool.objects.filter(was_damaged=True, lend_date__month=month, lend_date__year=year)
     return damaged_tools
 
 
-def get_orders_with_storage_fee():
-    orders = OrderPayment.objects.all().exclude(days_in_store=0)
+def get_damaged_tools_previous():
+    year = previous_month()['year']
+    month = previous_month()['pre_month']
+    damaged_tools = LentTool.objects.filter(was_damaged=True, lend_date__month=month, lend_date__year=year)
+    return damaged_tools
+
+
+def get_orders_with_storage_fee_current():
+    year = current_month()['year']
+    month = current_month()['month']
+    orders = OrderPayment.objects.filter(order__order_date__month=month, order__order_date__year=year).exclude(
+        days_in_store=0)
     return orders
+
+
+def get_orders_with_storage_fee_previous():
+    year = previous_month()['year']
+    month = previous_month()['pre_month']
+    orders = OrderPayment.objects.filter(order__order_date__month=month, order__order_date__year=year).exclude(
+        days_in_store=0)
+    return orders
+
+
+def get_current_returned_tools():
+    year = current_month()['year']
+    month = current_month()['month']
+    lent_tools = LentTool.objects.filter(lend_date__month=month, lend_date__year=year)
+    return lent_tools
+
+
+def get_previous_returned_tools():
+    year = previous_month()['year']
+    month = previous_month()['pre_month']
+    lent_tools = LentTool.objects.filter(lend_date__month=month, lend_date__year=year)
+    return lent_tools
--- furniture_palace/reports/views.py
+++ furniture_palace/reports/views.py
@@ -1,39 +1,74 @@
+import calendar
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from reports import reports
-from catalog.catalog import get_returned_lent_tools
 
 
 @login_required(login_url='/')
 def show_carpenter_summary(request):
-    carpenter_orders = reports.get_carpenter_orders(request.user)
-    carpenter_orders_commission_total = reports.get_carpenter_order_totals_commission(request.user)
-    assigned_orders = reports.get_assigned_orders(request.user)
-    assigned_orders_commission_total = reports.get_assigned_order_totals_commission(request.user)
-    total_commission = reports.total_commission(request.user)
+    # current carpenter summary
+    carpenter_orders = reports.get_carpenter_orders_current(request.user)
+    carpenter_orders_commission_total = reports.get_carpenter_order_totals_commission_current(request.user)
+    assigned_orders = reports.get_assigned_orders_current(request.user)
+    assigned_orders_commission_total = reports.get_assigned_order_totals_commission_current(request.user)
+    total_commission = reports.total_commission_current(request.user)
     salary = reports.get_salary(request.user)
-    gross_salary = reports.carpenter_gross_salary(request.user)
+    gross_salary = reports.carpenter_gross_salary_current(request.user)
     orders_commission = float(0.02)
     assigned_order_commission = float(0.01)
+
+    # previous carpenter summary
+    previous_carpenter_orders = reports.get_carpenter_orders_previous(request.user)
+    previous_carpenter_orders_commission_total = reports.get_carpenter_order_totals_commission_previous(request.user)
+    previous_assigned_orders = reports.get_assigned_orders_previous(request.user)
+    previous_assigned_orders_commission_total = reports.get_assigned_order_totals_commission_previous(request.user)
+    previous_total_commission = reports.total_commission_previous(request.user)
+    previous_salary = reports.get_salary(request.user)
+    previous_gross_salary = reports.carpenter_gross_salary_previous(request.user)
+
+    # getting the month name from its number
+    month = calendar.month_name[reports.current_month()['month']]
+    year = reports.current_month()['year']
+    previous_month = calendar.month_name[reports.previous_month()['pre_month']]
+    previous_year = reports.previous_month()['year']
     return render(request, 'reports/carpenter_report.html', locals())
 
 
 @login_required(login_url='/')
 def show_monthly_summary(request):
-    # lending_fees(pending)
-    # delivery_fees(pending)
+    # current month summary
     salary_totals = reports.get_salary_totals()
-    orders_totals = reports.get_orders_totals()
-    commission_totals = reports.get_commissions_grand_totals()
-    damage_fee_totals = reports.get_total_tool_damage_fees()
-    storage_fee_totals = reports.get_total_storage_fees()
-    lending_fee_totals = reports.get_total_lending_fees()
-
-    lent_tools = get_returned_lent_tools()
-    orders_with_storage_fee = reports.get_orders_with_storage_fee()
-    damaged_tools = reports.get_damaged_tools()
+    orders_totals = reports.get_orders_totals_current()
+    commission_totals = reports.get_commissions_grand_totals_current()
+    damage_fee_totals = reports.get_total_tool_damage_fees_current()
+    storage_fee_totals = reports.get_total_storage_fees_current()
+    lending_fee_totals = reports.get_total_lending_fees_current()
+    lent_tools = reports.get_current_returned_tools()
+    orders_with_storage_fee = reports.get_orders_with_storage_fee_current()
+    damaged_tools = reports.get_damaged_tools_current()
     carpenter_profiles = reports.get_carpenter_profiles()
-    complete_orders = reports.get_all_complete_orders()
-    carpenter_commission_totals = reports.get_carpenter_commissions_subtotals()
-    temp_carpenter_commission_totals = reports.get_temp_carpenter_commissions_subtotals()
+    complete_orders = reports.get_all_complete_orders_current()
+    carpenter_commission_totals = reports.get_carpenter_commissions_subtotals_current()
+    temp_carpenter_commission_totals = reports.get_temp_carpenter_commissions_subtotals_current()
+
+    # previous month summary
+    previous_salary_totals = reports.get_salary_totals()
+    previous_orders_totals = reports.get_orders_totals_previous()
+    previous_commission_totals = reports.get_commissions_grand_totals_previous()
+    previous_damage_fee_totals = reports.get_total_tool_damage_fees_previous()
+    previous_storage_fee_totals = reports.get_total_storage_fees_previous()
+    previous_lending_fee_totals = reports.get_total_lending_fees_previous()
+    previous_lent_tools = reports.get_previous_returned_tools()
+    previous_orders_with_storage_fee = reports.get_orders_with_storage_fee_previous()
+    previous_damaged_tools = reports.get_damaged_tools_previous()
+    previous_carpenter_profiles = reports.get_carpenter_profiles()
+    previous_complete_orders = reports.get_all_complete_orders_previous()
+    previous_carpenter_commission_totals = reports.get_carpenter_commissions_subtotals_previous()
+    previous_temp_carpenter_commission_totals = reports.get_temp_carpenter_commissions_subtotals_previous()
+
+    # getting the month name from its number
+    month = calendar.month_name[reports.current_month()['month']]
+    year = reports.current_month()['year']
+    previous_month = calendar.month_name[reports.previous_month()['pre_month']]
+    previous_year = reports.previous_month()['year']
     return render(request, 'reports/monthly_report.html', locals())
